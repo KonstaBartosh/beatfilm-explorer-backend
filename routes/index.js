1c93b8e7877d06ef9e71c6b3bdbdb4d189d5f288
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-07 13:42:47 +0700

    Feat: create routes for movies

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ const { register, login } = require('../controllers/user');
 const NotFoundError = require('../errors/NotFoundError');
 const auth = require('../middlewares/auth');
 const userRouter = require('./user');
+const movieRouter = require('./movie');
 
 //* * роуты не требующие авторизации */
 router.post('/signup', register);
 router.post('/signin', login);
+
 //* * авторизация */
 router.use(auth);
+
 //* * роуты, которым авторизация нужна */
 router.use('/users', userRouter);
+router.use('/movies', movieRouter);
 
 //* * Обработчик несуществующих маршрутов */
 router.use('*', (req, res, next) => {
