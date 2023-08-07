f003a5f60070b467f020a996b3ff84d2f76e14cc
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-07 16:12:19 +0700

    Feat: create logger for requests & errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/index');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -14,5 +15,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/diploma_db')
   .catch((err) => console.error('Error:', err));
 
 app.use(express.json());
-
+app.use(requestLogger);
 app.use(router);
+app.use(errorLogger);
