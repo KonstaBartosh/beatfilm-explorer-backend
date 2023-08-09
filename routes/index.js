2fbd5c746bea91b29a75bcd26a2dbd195a05c62f
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-09 19:50:06 +0700

    Feat: create auth limiter

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const rateLimit = require('express-rate-limit')
 
 const { register, login } = require('../controllers/user');
 const NotFoundError = require('../errors/NotFoundError');
@@ -7,9 +8,17 @@ const userRouter = require('./user');
 const movieRouter = require('./movie');
 const { registerValidation, loginValidation } = require('../middlewares/validation');
 
+//* * ограничитель запросов */
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Слишком много запросов, пожалуйста попробуйте позже',
+});
+
 //* * роуты не требующие авторизации */
-router.post('/signup', registerValidation, register);
-router.post('/signin', loginValidation, login);
+router.post('/signup', authLimiter, registerValidation, register);
+router.post('/signin', authLimiter, loginValidation, login);
 
 //* * авторизация */
 router.use(auth);
