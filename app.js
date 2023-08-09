601f1867afd63f742b5e486034097234201a5a6a
Konstantin Bartosh <rustigillo@gmail.com>
2023-08-09 10:43:35 +0700

    Refactor: check .env file with console

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-const { PORT, DB_ADRESS } = process.env;
+const { PORT, DB_ADRESS, TEST } = process.env;
 
 //* * запуск сервера express.js и прослушивание запросов в порту*/
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb')
-  .then(() => console.log('Connected to DB'))
+  .then(() => console.log(`Connected to DB${  TEST}`))
   .catch((err) => console.error('Error:', err));
 
 app.use(cors());
